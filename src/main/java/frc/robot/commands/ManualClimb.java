815b8c1659a9b497e5838aeb738f02c8de559578
Mark Zakharyan <markazakharyan@gmail.com>
2022-01-22 16:50:28 -0800

    modified epsilon & compconstant

---
+++ src/main/java/frc/robot/commands/ManualClimb.java
@@ -0,0 +1,57 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import frc.robot.subsystems.Climb;
+
+public class ManualClimb extends CommandBase {
+  /** Creates a new ManualClimb. */
+
+  public static interface Controls {
+    double getRotatorSpeed();
+    boolean extendPistons();
+    double setExtensionMotors();
+  }
+
+
+  private final Climb climb;
+  private final Controls controls;
+  
+
+  public ManualClimb(Climb climb, Controls controls) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.climb = climb;
+    this.controls = controls;
+    addRequirements(climb);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    climb.initialize();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    climb.runRotatorMotor(controls.getRotatorSpeed());
+    climb.extendPistons(controls.extendPistons());
+    climb.setTargetVelocity(controls.setExtensionMotors());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    climb.initialize();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
