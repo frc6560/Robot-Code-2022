2419e6c6dd4a6bdcc77c22c25239b1b36a3cbc76
markzakharyan <markazakharyan@gmail.com>
2022-02-13 10:49:10 -0800

    Revert "idek what i did"
    This reverts commit ae6cb7afe2996fb67e7d348ec7f003da6f9d1369.

--- src/main/java/frc/robot/commands/ManualIntake.java
+++ src/main/java/frc/robot/commands/ManualIntake.java
@@ -35,6 +35,7 @@ public class ManualIntake extends CommandBase {
   public void execute() {
     double output = 0;
     output = -controls.getIntakeRunning();
+    System.out.println(controls.isIntakePistonEngaged());
     this.intake.setPiston(controls.isIntakePistonEngaged());
     this.intake.setIntakeMotorOutput(output);
     this.intake.setHopperMotorOutput(output);
